125f132268d0884537df20a4f8517a8b16062053
Zhishe Chen <33152781+JakeChen6@users.noreply.github.com>
2020-06-14 22:52:34 +0800

    Create orderbook module

---
+++ orderbook/orderbook.py
@@ -0,0 +1,170 @@
+"""
+
+@author: Zhishe
+
+"""
+
+import time
+
+from redisOrderTree import OrderTree
+
+class OrderException(Exception): pass
+class OrderQuantityError(OrderException): pass
+class OrderPriceError(OrderException): pass
+
+
+class Order:
+    def __init__(self, orderId, qty, price, timestamp):
+        self.orderId = orderId
+        self.qty = float(qty)  # not sure whether crypto trading allows fractional size
+        self.price = float(price)
+        self.timestamp = timestamp
+
+    def processPriceLevel(self, book, tree, orderList, qtyToTrade):
+        """
+        Takes a list of quotes at a certain price level as well as an incoming order
+        and matches appropriate trades given the order quantity.
+        """
+        trades = []
+        for order in orderList:
+            if qtyToTrade <= 0:
+                break
+            if qtyToTrade < order.qty:
+                tradeQty = qtyToTrade
+                newBookQty = order.qty - qtyToTrade
+                tree.updateOrderQuantity(order.orderId, newBookQty)
+                qtyToTrade = 0
+            elif qtyToTrade == order.qty:
+                tradeQty = qtyToTrade
+                tree.removeOrderById(order.orderId)  # hit bid or lift ask
+                qtyToTrade = 0
+            else:
+                tradeQty = order.qty
+                tree.removeOrderById(order.orderId)  # hit bid or lift ask
+                qtyToTrade -= tradeQty
+
+            transactionRecord = {'timestamp': book.getTimestamp(), 'price': order.price, 'qty': tradeQty}
+            if tree.side == 'bid':
+                transactionRecord['party1'] = ['bid', order.orderId]
+                transactionRecord['party2'] = ['ask', None]
+            else:
+                transactionRecord['party1'] = ['ask', order.orderId]
+                transactionRecord['party2'] = ['bid', None]
+            trades.append(transactionRecord)
+        return qtyToTrade, trades
+
+
+class Bid(Order):
+    def __init__(self, orderId, qty, price, timestamp):
+        Order.__init__(self, orderId, qty, price, timestamp)
+        self.side = 'bid'
+
+    def limitOrder(self, book, bids, asks):
+        trades = []
+        orderInBook = None
+        qtyToTrade = self.qty
+        # __len__ of asks is called as __bool__ is not implemented
+        while (asks and self.price >= asks.minPrice() and qtyToTrade > 0):
+            bestPriceAsks = [Ask(x['orderId'], x['qty'], x['price'], x['timestamp']) for x in asks.minPriceList()]
+            qtyToTrade, newTrades = self.processPriceLevel(book, asks, bestPriceAsks, qtyToTrade)
+            trades += newTrades
+        # if volume remains, add to book
+        if qtyToTrade > 0:
+            self.qty = qtyToTrade
+            bids.insertOrder(self)
+            orderInBook = self
+        return trades, orderInBook
+
+    def marketOrder(self, book, bids, asks):
+        trades = []
+        qtyToTrade = self.qty
+        # __len__ of asks is called as __bool__ is not implemented
+        while qtyToTrade > 0 and asks:
+            bestPriceAsks = [Ask(x['orderId'], x['qty'], x['price'], x['timestamp']) for x in asks.minPriceList()]
+            qtyToTrade, newTrades = self.processPriceLevel(book, asks, bestPriceAsks, qtyToTrade)
+            trades += newTrades
+        return trades
+
+
+class Ask(Order):
+    def __init__(self, orderId, qty, price, timestamp):
+        Order.__init__(self, orderId, qty, price, timestamp)
+        self.side = 'ask'
+
+    def limitOrder(self, book, bids, asks):
+        trades = []
+        orderInBook = None
+        qtyToTrade = self.qty
+        # __len__ of bids is called as __bool__ is not implemented
+        while (bids and self.price <= bids.maxPrice() and qtyToTrade > 0):
+            bestPriceBids = [Bid(x['orderId'], x['qty'], x['price'], x['timestamp']) for x in bids.maxPriceList()]
+            qtyToTrade, newTrades = self.processPriceLevel(book, bids, bestPriceBids, qtyToTrade)
+            trades += newTrades
+        # if volume remains, add to book
+        if qtyToTrade > 0:
+            self.qty = qtyToTrade
+            asks.insertOrder(self)
+            orderInBook = self
+        return trades, orderInBook
+
+    def marketOrder(self, book, bids, asks):
+        trades = []
+        qtyToTrade = self.qty
+        # __len__ of bids is called as __bool__ is not implemented
+        while qtyToTrade > 0 and bids:
+            bestPriceBids = [Bid(x['orderId'], x['qty'], x['price'], x['timestamp']) for x in bids.maxPriceList()]
+            qtyToTrade, newTrades = self.processPriceLevel(book, bids, bestPriceBids, qtyToTrade)
+            trades += newTrades
+        return trades
+
+
+class OrderBook:
+    def __init__(self, exchange, symbol, red):
+        self.exch = exchange
+        self.symbol = symbol
+        self.red = red
+
+        self.bids = OrderTree(exchange, symbol, 'bid', red)
+        self.asks = OrderTree(exchange, symbol, 'ask', red)
+
+        self._lastTimestamp = None
+
+    def processOrder(self, order):
+        orderInBook = None
+
+        if order.qty <= 0:
+            raise OrderQuantityError('order.qty must be > 0')
+        if order.price <= 0:
+            raise OrderPriceError('order.price must be > 0')
+
+        trades, orderInBook = order.limitOrder(self, self.bids, self.asks)
+
+        return trades, orderInBook
+
+    def cancelOrder(self, side, orderId):
+        """delete
+        """
+        if side == 'bid':
+            self.bids.removeOrderById(orderId)
+        else:
+            self.asks.removeOrderById(orderId)
+
+    def getBestBid(self):
+        return self.bids.maxPrice()
+    
+    def getWorstBid(self):
+        return self.bids.minPrice()
+    
+    def getBestAsk(self):
+        return self.asks.minPrice()
+    
+    def getWorstAsk(self):
+        return self.asks.maxPrice()
+
+    def getTimestamp(self):
+        t = time.time()
+        while t == self._lastTimestamp:
+            t = time.time()
+        self._lastTimestamp = t
+        return t
+
---
+++ orderbook/redisOrderTree.py
@@ -0,0 +1,84 @@
+"""
+
+@author: Zhishe
+
+"""
+
+
+class OrderTree:
+    def __init__(self, exchange, symbol, side, red):
+        """
+        exchange : str
+        symbol: str
+        side : str
+        red : redis.Redis
+        """
+        self.exch = exchange
+        self.symbol = symbol
+        self.side = side
+        self.red = red
+
+        self.KEY_PRICE_TREE = '%s-%s-prices-%s' % (exchange, symbol, side)
+        self.KEY_TEMPLATE_QUOTE = '%s-%s-quote-%%s' % (exchange, symbol)  # quote id
+        self.KEY_TEMPLATE_QUOTES_BY_PRICE = '%s-%s-%s-%%s' % (exchange, symbol, side)  # price
+
+    def __len__(self):
+        return self.red.zcard(self.KEY_PRICE_TREE)
+
+    def getQuotesAtPrice(self, price):
+        return self.red.lrange(self.KEY_TEMPLATE_QUOTES_BY_PRICE % price, 0, -1)
+
+    def orderExists(self, orderId):
+        return self.red.exists(self.KEY_TEMPLATE_QUOTE % orderId)
+
+    def insertOrder(self, order):
+        """
+        order : Order
+        """
+        price = order.price
+        if not self.red.exists(self.KEY_TEMPLATE_QUOTES_BY_PRICE % price):
+            self.red.zadd(self.KEY_PRICE_TREE, {price: price})
+
+        self.red.hset(self.KEY_TEMPLATE_QUOTE % order.orderId, mapping=order.__dict__)
+        self.red.rpush(self.KEY_TEMPLATE_QUOTES_BY_PRICE % price, order.orderId)
+
+    def updateOrderQuantity(self, orderId, newQty):
+        self.red.hset(self.KEY_TEMPLATE_QUOTE % orderId, 'qty', newQty)
+
+    def removeOrderById(self, orderId):
+        order = self.red.hgetall(self.KEY_TEMPLATE_QUOTE % orderId)
+        self.red.lrem(self.KEY_TEMPLATE_QUOTES_BY_PRICE % order['price'], 0, orderId)
+        if not self.red.exists(self.KEY_TEMPLATE_QUOTES_BY_PRICE % order['price']):
+            self.red.zrem(self.KEY_PRICE_TREE, order['price'])
+        self.red.delete(self.KEY_TEMPLATE_QUOTE % orderId)
+
+    def maxPrice(self):
+        r = self.red.zrevrange(self.KEY_PRICE_TREE, 0, 0)
+        if r:
+            return float(r[0])
+        else:
+            return 0
+    
+    def minPrice(self):
+        r = self.red.zrange(self.KEY_PRICE_TREE, 0, 0)
+        if r:
+            return float(r[0])
+        else:
+            return 0
+
+    def maxPriceList(self):
+        # in redis-py, pipeline is a transactional pipeline class by default
+        with self.red.pipeline() as pipe:
+            for orderID in self.red.lrange(self.KEY_TEMPLATE_QUOTES_BY_PRICE % self.maxPrice(), 0, -1):
+                pipe.hgetall(self.KEY_TEMPLATE_QUOTE % orderID)
+            res = pipe.execute()
+        return res
+
+    def minPriceList(self):
+        # in redis-py, pipeline is a transactional pipeline class by default
+        with self.red.pipeline() as pipe:
+            for orderID in self.red.lrange(self.KEY_TEMPLATE_QUOTES_BY_PRICE % self.minPrice(), 0, -1):
+                pipe.hgetall(self.KEY_TEMPLATE_QUOTE % orderID)
+            res = pipe.execute()
+        return res
+
